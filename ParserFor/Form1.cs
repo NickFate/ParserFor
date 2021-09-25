87c942280674e7f4116c1a9f3e0acc808cb2eef8
NickFate <fategraffit@gmail.com>
2021-09-25 23:42:59 +0300

    l

--- ParserFor/Form1.cs
+++ ParserFor/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,24 @@ namespace ParserFor
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string irl = textBox1.Text;
+
+            string irl = "https://github.com/NickFate/ParserFor";
+            
+            WebClient client = new WebClient();
+            string res = client.DownloadString(irl);
+
+            res = res.Substring(res.IndexOf("<title>") + 7);
+            res = res.Replace(res.Substring(res.IndexOf("</title>")), "");
+            res = res.Substring(res.IndexOf("/") + 1);
+            res = res.Replace(res.Substring(res.IndexOf(":")), "");
+
+            textBox2.Text = res;
+
         }
     }
 }
